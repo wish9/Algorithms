45d008bd498fd81bf93b4becd1b45dce2de21ad2
wish9 <wjwee9@gmail.com>
2023-06-01 02:14:47 +0900

    2070: 값 비교해서 부등호 출력하기 (중복되는 코드 수정)

--- SW_Expert_Academy/d1/2070InequalitySign.py
+++ SW_Expert_Academy/d1/2070InequalitySign.py
@@ -1,12 +1,11 @@
 T = int(input())
 
 for t in range(1, T + 1):
-    values = list(map(int, input().split()))
-    x = values[0]
-    y = values[1]
-    if(x<y) :
-        print("#%s <" %str(t))
-    elif(x==y) :
-        print("#%s =" %str(t))
-    else :
-        print("#%s >" %str(t))
+    x, y = map(int, input().split())
+    print("#%d" %(t), end=' ')
+    if x < y:
+        print("<")
+    elif x == y:
+        print("=")
+    else:
+        print(">")
