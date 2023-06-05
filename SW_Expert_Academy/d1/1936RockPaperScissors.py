59732d64866bf670c574b0f526d008883cecd808
wish9 <wjwee9@gmail.com>
2023-06-05 23:39:06 +0900

    1936: 가위바위보 - %연산자 사용 단순화 (무승부 없음)

--- SW_Expert_Academy/d1/1936RockPaperScissors.py
+++ SW_Expert_Academy/d1/1936RockPaperScissors.py
@@ -7,7 +7,7 @@ values = list(map(int, input().split()))
 #     if values[0]==2 or values[1]==2 : print('A')
 #     else: print('B')
 
-if values[0] % 3 == (values[1]-1) % 3:
+if (values[1]-values[0]+3) % 3 == 1:
     print('B')
 else:
     print('A')
