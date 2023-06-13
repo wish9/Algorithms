21ace7ab45fa8e38d3de1baa5520e0d7c89f6ab7
wish9 <wjwee9@gmail.com>
2023-06-13 23:20:02 +0900

    1288: 나온 숫자 카운팅 (새로운 불면증 치료법)

---
+++ SW_Expert_Academy/d2/1288CountingSheep.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+for t in range(1, T + 1):
+    print('#%d' %t, end=' ')
+    countingNumList = [False]*10
+    n = int(input())
+
+    for i in range(1, 10**6):
+        nowNum = i * n
+        digits = [int(digit) for digit in str(nowNum)] # 숫자를 자리수로 분리
+        false_index = [index for index in range(len(countingNumList)) if not countingNumList[index]] # 아직 못본 숫자
+
+        common = [value for value in digits if value in false_index] # 새로 보게 된 숫자
+        for j in common:
+            countingNumList[j] = True
+
+        if all(countingNumList):
+            print(nowNum)
+            break
