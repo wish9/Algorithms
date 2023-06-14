4b8d9d0b2c8377179625c9d99058f335a3250d16
wish9 <wjwee9@gmail.com>
2023-06-15 01:10:18 +0900

    1945: 간단한 소인수분해

--- SW_Expert_Academy/d2/1945PrimeFactorization.py
+++ SW_Expert_Academy/d2/1945PrimeFactorization.py
@@ -1,17 +1,31 @@
-def Square_of_x(x, N):
-    result = 0
-    if N%x == 0:
-        N = N/x
-        result += (1 + Square_of_x(x, N))
-    return result
+# def Square_of_x(x, N):
+#     result = 0
+#     if N%x == 0:
+#         N = N/x
+#         result += (1 + Square_of_x(x, N))
+#     return result
+#
+# T = int(input())
+#
+# for t in range(1, T+1):
+#     print("#%d" %t, end=" ")
+#     N = int(input())
+#
+#     for i in [2, 3, 5, 7, 11]:
+#         result = Square_of_x(i, N)
+#         print(result, end=" ")
+#     print("")
 
 T = int(input())
-
 for t in range(1, T+1):
-    print("#%d" %t, end=" ")
     N = int(input())
-
-    for i in [2, 3, 5, 7, 11]:
-        result = Square_of_x(i, N)
-        print(result, end=" ")
-    print("")
+    small_arguments = [2, 3, 5, 7, 11]
+    count = [0] * len(small_arguments)
+    for n in range(len(small_arguments)):
+        while True:
+            if N % small_arguments[n] == 0:
+                N = N//small_arguments[n]
+                count[n] += 1
+            else:
+                break
+    print('#{}'.format(t), ' '.join(map(str, count)))
