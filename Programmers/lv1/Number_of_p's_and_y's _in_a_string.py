bd35349ae36d0588f338a8dc8bccee2fcd5b1a28
wish9 <wjwee9@gmail.com>
2023-06-22 23:59:22 +0900

    lv1: 문자열 내 p와 y의 개수

--- Programmers/lv1/Number_of_p's_and_y's _in_a_string.py
+++ Programmers/lv1/Number_of_p's_and_y's _in_a_string.py
@@ -2,10 +2,14 @@ s = input()
 Num_of_P = 0
 Num_of_Y = 0
 
+# for i in range(len(s)):
+#     if s[i]=='P' or s[i]=='p': Num_of_P += 1
+#     elif s[i]=='Y' or s[i]=='y': Num_of_Y += 1
+#
+# if Num_of_P == Num_of_Y: print(True)
+# else: print(False)
 
-for i in range(len(s)):
-    if s[i]=='P' or s[i]=='p': Num_of_P += 1
-    elif s[i]=='Y' or s[i]=='y': Num_of_Y += 1
+def numPY(s):
+    return s.lower().count('p') == s.lower().count('y') # 대소문자 상관 없이 카운팅 하기 위해 lower 사용
 
-if Num_of_P == Num_of_Y: print(True)
-else: print(False)
+print(numPY(s))
