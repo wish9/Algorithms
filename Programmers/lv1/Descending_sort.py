6a9c0b5a9427b8f3ae3552c7fb45cca3450692ba
wish9 <wjwee9@gmail.com>
2023-06-25 01:24:53 +0900

    lv1: 정수 내림차순으로 배치하기 (두번쨰 풀이)

--- Programmers/lv1/Descending_sort.py
+++ Programmers/lv1/Descending_sort.py
@@ -8,4 +8,10 @@ def solution(n):
         n = n.replace(str(i), '')
     return int(result)
 
+def solution2(n):
+    ls = list(str(n))
+    ls.sort(reverse = True)
+    return int("".join(ls))
+
 print(solution(ip))
+print(solution2(ip))
