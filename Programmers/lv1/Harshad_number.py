080716eda9f8e003d9984876bae3a6a352ee75ee
wish9 <wjwee9@gmail.com>
2023-06-26 23:11:55 +0900

    lv1: 하샤드 수

---
+++ Programmers/lv1/Harshad_number.py
@@ -0,0 +1,17 @@
+ip = int(input())
+
+'''
+하샤드 수가 맞는지 판단하는 메서드
+* 하샤드 수란 각 자리수 합으로 나누어 떨어지는 수
+'''
+def solution(n):
+    n = str(n)
+    sum_each_digit = 0
+    for i in range(len(n)):
+        sum_each_digit += int(n[i])
+
+    if int(n)%sum_each_digit==0: return True
+
+    return False
+
+print(solution(ip))
