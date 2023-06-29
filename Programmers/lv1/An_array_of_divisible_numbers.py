ff6ccd7082a2da995c45c5216f360d1cf9c4d052
wish9 <wjwee9@gmail.com>
2023-06-29 18:51:31 +0900

    lv1: 나누어 떨어지는 숫자 배열 (두번째 풀이)

--- Programmers/lv1/An_array_of_divisible_numbers.py
+++ Programmers/lv1/An_array_of_divisible_numbers.py
@@ -10,5 +10,9 @@ def solution(arr, divisor):
     if len(answer)==0: answer.append(-1)
     return answer
 
+def solution2(arr, divisor):
+    return sorted([n for n in arr if n%divisor == 0]) or [-1]
+
 # print(solution(arr, divisor).sort()) # sort는 제자리 메서드라 이런식으로 쓰면 None이 나와버림
 print(solution(arr, divisor))
+print(solution2(arr, divisor))
