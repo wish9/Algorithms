d944c83b5d984a9675ba0c2e424a15fe6d29b521
wish9 <wjwee9@gmail.com>
2023-07-09 16:42:30 +0900

    lv2: 영어 끝말잇기 (두번쨰 풀이)

--- Programmers/lv2/English_Endings.py
+++ Programmers/lv2/English_Endings.py
@@ -1,5 +1,5 @@
-n = int(input())
-words = input().strip('[]').replace('"', "").split(', ')
+# n = int(input())
+# words = input().strip('[]').replace('"', "").split(', ')
 
 def solution(n, words):
     answer = []
@@ -27,5 +27,34 @@ def solution(n, words):
 
     return answer
 
-print(solution(n, words))
-print(words)
+def solution2(n, words):
+    answer = []
+    usedWords = []
+    wrongIndex = 0
+
+    for i in range(len(words)):
+        now = words[i]
+        if i > 0 and (now[0] != words[i-1][-1] or now in usedWords):
+            wrongIndex = i
+            break
+        usedWords.append(now)
+
+    if wrongIndex == 0:
+        answer.extend([0, 0])
+    else:
+        answer.append(wrongIndex % n + 1)
+        answer.append(wrongIndex // n + 1)
+
+    return answer
+
+
+# print(solution(n, words))
+# print(solution2(n, words))
+
+print(solution(3 ,["tank", "kick", "know", "wheel", "land",
+                   "dream", "mother", "robot", "tank"]))
+print(solution(5, ["hello", "observe", "effect", "take", "either",
+                   "recognize", "encourage", "ensure", "establish",
+                   "hang", "gather", "refer", "reference", "estimate",
+                   "executive"]))
+print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
