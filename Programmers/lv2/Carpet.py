5f4dcd5b6e42cbcff1e5bf98a9379c93319dc9ec
wish9 <wjwee9@gmail.com>
2023-07-10 22:55:09 +0900

    lv2: 카펫 [틀린풀이] (69.2/100)

---
+++ Programmers/lv2/Carpet.py
@@ -0,0 +1,29 @@
+def commonNumbers(n):
+    answer = []
+    answer.append(1)
+
+    for i in range(2,n//2+1,2):
+        if n%i==0: answer.append(i)
+
+    if n != 1:
+        answer.append(n)
+
+    return answer
+
+def solution(brown, yellow):
+    answer = []
+    yellowCommonNumbers = commonNumbers(yellow)
+
+    for i in yellowCommonNumbers:
+        transverse = yellow//i+2
+        length = i+2
+        if transverse*length == brown+yellow and transverse >= length:
+            answer.append(transverse)
+            answer.append(length)
+
+    return answer
+
+
+print(solution(10,2))
+print(solution(8,1))
+print(solution(24,24))
