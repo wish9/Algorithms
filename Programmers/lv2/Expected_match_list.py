835ab86494f33a6d7f6f0100d5d678d67fb0e3d2
wish9 <wjwee9@gmail.com>
2023-07-11 12:36:22 +0900

    lv2: 예상 대진표 (두번째 풀이)

--- Programmers/lv2/Expected_match_list.py
+++ Programmers/lv2/Expected_match_list.py
@@ -26,6 +26,12 @@ def solution(n, a, b):
 
     return answer
 
-
+def solution2(n,a,b):
+    answer = 0
+    while a != b:
+        answer += 1
+        a, b = (a+1)//2, (b+1)//2
+    return answer
 
 print(solution(8,4,7))
+print(solution2(8,4,7))
