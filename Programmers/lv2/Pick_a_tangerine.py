1f9b190a54e06f16c9bbbc83d591d83055ccbea6
wish9 <wjwee9@gmail.com>
2023-07-17 22:43:52 +0900

    lv2: 귤 고르기 (두번째 풀이)

--- Programmers/lv2/Pick_a_tangerine.py
+++ Programmers/lv2/Pick_a_tangerine.py
@@ -1,3 +1,5 @@
+import collections
+
 def solution(k, tangerine):
     answer = 0
     tangerine.sort()
@@ -27,6 +29,21 @@ def solution(k, tangerine):
 
     return answer
 
+def solution2(k, tangerine):
+    answer = 0
+    cnt = collections.Counter(tangerine)
+
+    for v in sorted(cnt.values(), reverse = True):
+        k -= v
+        answer += 1
+        if k <= 0:
+            break
+    return answer
+
 print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3]))
 print(solution(4, [1, 3, 2, 5, 4, 5, 2, 3]))
 print(solution(2, [1, 1, 1, 1, 2, 2, 2, 3]))
+print('-'*30)
+print(solution2(6, [1, 3, 2, 5, 4, 5, 2, 3]))
+print(solution2(4, [1, 3, 2, 5, 4, 5, 2, 3]))
+print(solution2(2, [1, 1, 1, 1, 2, 2, 2, 3]))
