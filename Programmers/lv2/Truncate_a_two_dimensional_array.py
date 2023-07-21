373b3b09d4f858191224996af66b6a45ad532fe4
wish9 <wjwee9@gmail.com>
2023-07-21 12:55:41 +0900

    lv2: n^2 배열 자르기 [시간초과 30/100]

--- Programmers/lv2/Truncate_a_two_dimensional_array.py
+++ Programmers/lv2/Truncate_a_two_dimensional_array.py
@@ -1,4 +1,4 @@
-def solution(n, left, right):
+def fail(n, left, right):
     arr = []
     for i in range(1, n+1):
         for j in range(1, n+1):
@@ -8,4 +8,19 @@ def solution(n, left, right):
                 arr.append(j)
     return arr[left:right+1]
 
+def solution(n, left, right):
+
+    arr = [[0]*n for x in range(n)]
+
+    for i in range(0, n):
+        j = i
+
+        while j >= 0:
+            arr[i][j] = i + 1
+            arr[j][i] = i + 1
+            j -= 1
+
+    return sum(arr, [])[left:right+1]
+
 print(solution(3,2,5))
+print(solution(4,7,14))
