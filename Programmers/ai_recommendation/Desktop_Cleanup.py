119df237ce04feab592eefd083333cdcb353c27f
wish9 <wjwee9@gmail.com>
2023-07-24 02:38:41 +0900

    AI 추천문제: 바탕화면 정리 (두번째 풀이)

--- Programmers/ai_recommendation/Desktop_Cleanup.py
+++ Programmers/ai_recommendation/Desktop_Cleanup.py
@@ -18,5 +18,17 @@ def solution(wallpaper):
 
     return answer
 
-# print(solution([".#...", "..#..", "...#."]))
+def solution2(wall):
+    a, b = [], []
+    for i in range(len(wall)):
+        for j in range(len(wall[i])):
+            if wall[i][j] == "#":
+                a.append(i)
+                b.append(j)
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
+
+print(solution([".#...", "..#..", "...#."]))
 print(solution(["..........", ".....#....", "......##..", "...##.....", "....#....."]))
+
+print(solution2([".#...", "..#..", "...#."]))
+print(solution2(["..........", ".....#....", "......##..", "...##.....", "....#....."]))
