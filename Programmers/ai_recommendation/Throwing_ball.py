968a416d75d0c6126f019e1072ca07bd8659d00e
wish9 <wjwee9@gmail.com>
2023-07-28 10:11:07 +0900

    AI 추천문제: 공던지기 (두번째 풀이)

--- Programmers/ai_recommendation/Throwing_ball.py
+++ Programmers/ai_recommendation/Throwing_ball.py
@@ -2,6 +2,13 @@ def solution(numbers, k):
     answer = 1 + (k - 1) * 2
     return answer%len(numbers) if answer % len(numbers) else len(numbers)
 
+def solution2(numbers, k):
+    return (k - 1) * 2 % len(numbers) + 1
+
 print(solution([1, 2, 3, 4], 2))
 print(solution([1, 2, 3, 4, 5, 6], 5))
 print(solution([1, 2, 3], 3))
+print('-'*10)
+print(solution2([1, 2, 3, 4], 2))
+print(solution2([1, 2, 3, 4, 5, 6], 5))
+print(solution2([1, 2, 3], 3))
