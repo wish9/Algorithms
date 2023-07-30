6e9a08ef8b5e7909cbd8e1fb1a7ff245f7cc1c4d
wish9 <wjwee9@gmail.com>
2023-07-30 21:31:57 +0900

    lv2: 의상

--- Programmers/lv2/Clothes.py
+++ Programmers/lv2/Clothes.py
@@ -1,19 +1,14 @@
-from itertools import combinations
-import numpy as np
+from collections import Counter
 
 def solution(clothes):
-    answer = 0
 
-    from collections import Counter
     clothes_dict = Counter([item[1] for item in clothes])
 
-    arr = clothes_dict.values()
+    answer = 1
+    for num in clothes_dict.values():
+        answer *= (num + 1)
 
-    for r in range(len(arr) + 1):
-        for subset in combinations(arr, r):
-            answer += np.prod(subset)
-
-    return int(answer) - 1
+    return answer - 1
 
 print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
 print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))
