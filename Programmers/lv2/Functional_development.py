152276c7517c4dbc3de87c77640a80fdc303a694
wish9 <wjwee9@gmail.com>
2023-08-01 10:12:05 +0900

    lv2: 기능개발 (두번째 풀이)

--- Programmers/lv2/Functional_development.py
+++ Programmers/lv2/Functional_development.py
@@ -17,5 +17,15 @@ def solution(progresses, speeds):
             answer.append(result)
     return answer
 
+def solution2(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
+        else:
+            Q[-1][1]+=1
+
+    return [q[1] for q in Q]
+
 print(solution([93, 30, 55], [1, 30, 5]))
 print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
