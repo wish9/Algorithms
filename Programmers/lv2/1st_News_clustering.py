da1a539e907163a1e48000f5a65a2210d23778d6
wish9 <wjwee9@gmail.com>
2023-08-03 03:30:30 +0900

    lv2: [1차] 뉴스 클러스터링 (두번째 풀이)

--- Programmers/lv2/1st_News_clustering.py
+++ Programmers/lv2/1st_News_clustering.py
@@ -30,7 +30,29 @@ def solution(str1, str2):
 
     return answer
 
+import math
+
+def solution2(str1, str2):
+    str1 = [str1[i:i+2].lower() for i in range(0, len(str1)-1) if not re.findall('[^a-zA-Z]+', str1[i:i+2])]
+    str2 = [str2[i:i+2].lower() for i in range(0, len(str2)-1) if not re.findall('[^a-zA-Z]+', str2[i:i+2])]
+
+    gyo = set(str1) & set(str2)
+    hap = set(str1) | set(str2)
+
+    if len(hap) == 0 :
+        return 65536
+
+    gyo_sum = sum([min(str1.count(gg), str2.count(gg)) for gg in gyo])
+    hap_sum = sum([max(str1.count(hh), str2.count(hh)) for hh in hap])
+
+    return math.floor((gyo_sum/hap_sum)*65536)
+
 print(solution("FRANCE", "french"))
 print(solution("handshake", "shake hands"))
 print(solution("aa1+aa2", "AAAA12"))
 print(solution("E=M*C^2", "e=m*c^2"))
+print()
+print(solution2("FRANCE", "french"))
+print(solution2("handshake", "shake hands"))
+print(solution2("aa1+aa2", "AAAA12"))
+print(solution2("E=M*C^2", "e=m*c^2"))
