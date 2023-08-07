44549e64c5cfc4cd34fe7cf016965c408d63abc1
wish9 <wjwee9@gmail.com>
2023-08-07 22:49:44 +0900

    lv2: 전화번호 목록 [시간초과]

---
+++ Programmers/lv2/Lis_of_phone_numbers.py
@@ -0,0 +1,16 @@
+def solution(phone_book):
+    answer = True
+
+    sorted_phone_book = sorted(phone_book, key=len)
+
+    for i in range(len(phone_book)-1):
+        phone_num = sorted_phone_book[i]
+        for j in range(i+1, len(phone_book)):
+            if sorted_phone_book[j].startswith(phone_num):
+                answer = False
+
+    return answer
+
+print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123","456","789"]))
+print(solution(["12","123","1235","567","88"]))
