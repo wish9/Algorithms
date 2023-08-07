02a5e05bc45542c55ffd6d4d2dedcc0a2213015a
wish9 <wjwee9@gmail.com>
2023-08-07 23:01:27 +0900

    lv2: 전화번호 목록

--- Programmers/lv2/Lis_of_phone_numbers.py
+++ Programmers/lv2/Lis_of_phone_numbers.py
@@ -1,4 +1,4 @@
-def solution(phone_book):
+def fail(phone_book):
     answer = True
 
     sorted_phone_book = sorted(phone_book, key=len)
@@ -13,6 +13,15 @@ def solution(phone_book):
             break
 
     return answer
+
+def solution(phone_book):
+    phone_book.sort()
+
+    for i in range(len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+
+    return True
 
 print(solution(["119", "97674223", "1195524421"]))
 print(solution(["123","456","789"]))
