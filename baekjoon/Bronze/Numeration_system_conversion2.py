f074064fef9379c1d7ccf27c9decdb7c9a765cec
wish9 <wjwee9@gmail.com>
2023-08-08 21:50:47 +0900

    브론즈2: 진법 변환2 (두번째 풀이)

--- baekjoon/Bronze/Numeration_system_conversion2.py
+++ baekjoon/Bronze/Numeration_system_conversion2.py
@@ -23,3 +23,22 @@ def solution(N, B):
 print(solution(60466175, 36))
 print(solution(4, 2))
 print(solution(N, B))
+
+# 두번째 풀이
+# 오차범위 내지만 8ms 더 빠름
+# 기능분리가 더 잘됨
+n, b = map(int, input().split())
+
+
+def change(num):
+    if num < 10:
+        return str(num)
+    return str(chr(num + 55))
+
+result = ""
+
+while n >= b:
+    n, r = n // b, n % b
+    result += str(change(r))
+result += str(change(n))
+print(result[::-1])
