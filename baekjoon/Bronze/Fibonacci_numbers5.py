dcecd9c1eb8decf80639ffb270543aac675bd3da
wish9 <wjwee9@gmail.com>
2023-08-21 21:59:27 +0900

    브론즈2: 피보나치 수 5 (두번째 풀이)

--- baekjoon/Bronze/Fibonacci_numbers5.py
+++ baekjoon/Bronze/Fibonacci_numbers5.py
@@ -7,3 +7,10 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 
 print(fibonacci(ip))
+
+n = int(input())
+dp = [0,1]
+
+for i in range(1,n):
+    dp.append(dp[i]+dp[i-1])
+print(dp[n])
