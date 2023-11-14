c4182f87986e844ab32f7a8fcefb1461fc8bde34
wish9 <wjwee9@gmail.com>
2023-11-14 19:59:06 +0900

    1213: String

---
+++ SW_Expert_Academy/d3/1213_String.py
@@ -0,0 +1,18 @@
+for _ in range(10):
+    t = int(input())
+    searchS = input()
+    S = input()
+    result = 0
+
+    for i in range(len(S)-len(searchS)+1):
+        isSame = True
+        for j in range(len(searchS)):
+            if S[i+j] != searchS[j]:
+                isSame = False
+                break
+            else:
+                isSame = True
+        if isSame:
+            result += 1
+
+    print('#', t, ' ', result, sep='')
