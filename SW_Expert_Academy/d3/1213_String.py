7d9e549bd7311dfec63b5f4fba4f3985e1807d39
wish9 <wjwee9@gmail.com>
2023-11-14 19:59:43 +0900

    1213: String (두번째 풀이)

--- SW_Expert_Academy/d3/1213_String.py
+++ SW_Expert_Academy/d3/1213_String.py
@@ -1,18 +1,30 @@
+# for _ in range(10):
+#     t = int(input())
+#     searchS = input()
+#     S = input()
+#     result = 0
+#
+#     for i in range(len(S)-len(searchS)+1):
+#         isSame = True
+#         for j in range(len(searchS)):
+#             if S[i+j] != searchS[j]:
+#                 isSame = False
+#                 break
+#             else:
+#                 isSame = True
+#         if isSame:
+#             result += 1
+#
+#     print('#', t, ' ', result, sep='')
+
 for _ in range(10):
     t = int(input())
     searchS = input()
     S = input()
     result = 0
 
-    for i in range(len(S)-len(searchS)+1):
-        isSame = True
-        for j in range(len(searchS)):
-            if S[i+j] != searchS[j]:
-                isSame = False
-                break
-            else:
-                isSame = True
-        if isSame:
+    for i in range(len(S) - len(searchS) + 1):
+        if S[i:i+len(searchS)] == searchS:
             result += 1
 
     print('#', t, ' ', result, sep='')
